5a95cb7f1acdfdf2ba3542ee5e07500bf06b66fb
paulsumskoy <p.sumskoy@gmail.com>
2021-12-07 12:30:12 +0200

    Skin choose added

--- multiplayer/public/main.js
+++ multiplayer/public/main.js
@@ -1,10 +1,40 @@
-var input;
-var button;
-var name;
-var selectSkin;
+let input;
+let button;
+let playerName;
+let selectSkin;
 function setup() {
   createCanvas(480, 120);
   resetSetup();
   var button = createButton("reset");
   button.mousePressed(resetSetup);
+}
+
+function resetSetup() {
+  input = createInput();
+  input.position(20, 150);
+  button = createButton("submit");
+  button.position(160, 150);
+  button.mousePressed(drawName);
+
+  selectSkin = createSelect();
+  selectSkin.option('paul');
+  selectSkin.option('nikita');
+  selectSkin.option('sasha');
+  
+  background(100);
+  noStroke();
+  text("Enter your name.", 20, 20);
+}
+
+function drawName() {
+  background(100);
+  textSize(30);
+  playerName = input.value();
+  for (var i=0; i < 30; i++) {
+    fill(random(255));
+    text(playerName, random(width), random(height));
+  }
+  localStorage["name"] = playerName;
+  localStorage["skin"] = selectSkin.value();
+  window.location = "game/index.html";
 }
